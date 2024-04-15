238af550759305956caa303dc24d6dc031cd5b5c
Hoang Ha <SmithMerch2301@gmail.com>
2024-04-16 01:40:57 +0700

    Depot Scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+
+Product.create!(
+  title: 'Design and Build Great Web APIs',
+  description:
+    %{<p>
+      <em>Robust, Reliable, and Resilient</em>
+      APIs are transforming the business world at an increasing pace. Gain
+      the essential skills needed to quickly design, build, and deploy
+      quality web APIs that are robust, reliable, and resilient. Go from
+      initial design through prototyping and implementation to deployment of
+      mission-critical APIs for your organization. Test, secure, and deploy
+      your API with confidence and avoid the “release into production”
+      panic. Tackle just about any API challenge with more than a dozen
+      open-source utilities and common programming patterns you can apply
+      right away.
+    </p>},
+  image_url: 'maapis.jpg',
+  price: 24.95
+)
